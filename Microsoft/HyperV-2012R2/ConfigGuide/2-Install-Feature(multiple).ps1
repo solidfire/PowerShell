e0ff8a6cc09fd12cfee9f6ddc97219577dbd34da
Aaron <aaron.patten@solidfire.com>
2015-11-30 12:38:31 -0700

    PowerShell Hyper-V Configuration Examples
    Collection of example PowerShell scripts for configuring Hyper-V as
    described in the Configuring Windows Server 2012 R2 Hyper-V for
    SolidFire document

---
+++ Microsoft/HyperV-2012R2/ConfigGuide/2-Install-Feature(multiple).ps1
@@ -0,0 +1,36 @@
+﻿#Installs feature(s) on multiple hosts
+#pull in csv with server IP information
+$csvfile = "\\172.27.100.7\share\scripts\powershell\hyper-v\hyperv-ips.csv"
+$csv = Import-Csv -path $csvfile 
+
+#Get credentials for Invoke-Command 
+$creds = Get-Credential
+
+#Populate the servers and sort the array by the FQDN of the VMM hosts
+$servers = ($csv).hostname
+
+#List of features to install. If more than one, use comma separated list
+#Example for single feature $featurename = "GPMC"
+#Example for multiple features $featurename = "GPMC","Multipath-IO"
+$featurename = "GPMC","Failover-Clustering"
+
+#Iterate through all the servers in the $servers variable
+foreach ($server in $servers) {
+
+#For each feature, check if the feature is installed or not
+    foreach ($feature in $featurename) {
+    write-host "Checking for $feature on server $server"
+    $check = Get-WindowsFeature -Name $feature -ComputerName $server
+    
+        if ($check.Installed -ne "True") {
+        #Install the feature if not installed as well as management tools
+        Write-Host "Windows feature $feature not installed on server $server. Installing $feature ..." -ForegroundColor Green
+        Add-WindowsFeature $feature -ComputerName $server -IncludeManagementTools | Out-Null
+        }
+        else {
+        Write-Host "Feature $feature is already installed on host $server" -ForegroundColor Gray
+        }
+    }
+}
+
+Write-Host "Script Complete. Please reboot all hosts before continuing configuration"
---
+++ Microsoft/HyperV-2012R2/ConfigGuide/5-AddNewIscsiTargets.ps1
@@ -0,0 +1,45 @@
+﻿#Configures the iSCSI initiator on SCVMM managed hosts
+
+#pull in csv with server IP information
+$csvfile = "\\172.27.100.7\share\scripts\powershell\hyper-v\hyperv-ips.csv"
+$csv = Import-Csv -path $csvfile 
+
+#Get credentials for Invoke-Command 
+$creds = Get-Credential
+
+#Populate the servers 
+$servers = ($csv).hostname
+
+#Iterate through all the servers in the $servers variable
+foreach ($server in $servers) {
+
+    Invoke-Command -ComputerName $server -Credential $creds -ScriptBlock {
+
+        #Import IscsiTarget module if not loaded
+        if (Get-Module -ListAvailable -Name IscsiTarget) {
+            Write-Host "IscsiTarget module loaded... Continuting"
+        } else {
+            Write-Host "IscsiTarget module not loaded. Loading..."
+            Import-Module IscsiTarget
+        } #End if 
+        
+        #Set name variables for Invoke-Command
+        $temphost = hostname
+        $thishost = $using:csv | where{$_.hostname -eq $temphost}
+
+        #Message what server we are working on
+        Write-Host "########################################" -ForegroundColor Green
+        Write-Host "Beginning configuration of $using:server`r`n" -ForegroundColor Green
+
+        Write-Host "Updating iSCSI Targets..."
+        Update-IscsiTargetPortal -TargetPortalAddress $thishost.svip
+
+        Write-Host "Connecting any new inactive iSCSI targets on" $temphost -ForegroundColor DarkGray
+        $targets = Get-IscsiTarget | Where-Object {$_.IsConnected -ne "True"}
+        foreach ($target in $targets) {
+        Connect-IscsiTarget -NodeAddress $target.NodeAddress -InitiatorPortalAddress $thishost.ipiscsi1 -IsMultipathEnabled $true -IsPersistent $true -TargetPortalAddress $thishost.svip
+        Connect-IscsiTarget -NodeAddress $target.NodeAddress -InitiatorPortalAddress $thishost.ipiscsi2 -IsMultipathEnabled $true -IsPersistent $true -TargetPortalAddress $thishost.svip
+        Start-Sleep -s 1
+        } #End foreach ($target in $targets) loop
+    } #End Invoke-Command
+}#End foreach ($server in $servers) loop
