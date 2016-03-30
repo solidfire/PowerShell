650f176d7cec23a98af5c2d38eb287ac4041eaea
Josh Atwell <josh.c.atwell@gmail.com>
2016-03-30 09:24:48 -0600

    Updated steps on removing volumes from vSphere
    Providing updated steps on how to remove a volume. This now includes
    unmount and detach steps.

---
+++ VMware/Remove-SolidFire-Volume-From-VMware.ps1
@@ -0,0 +1,33 @@
+﻿# Load Datastore Functions
+. "C:\Users\jatwell\Google Drive\Scripts\DatastoreFunctions.ps1"
+
+# Load SPBM Module since we'll use the Get-SVolumeFromDatastore function.
+Import-Module "C:\Users\jatwell\Documents\GitHub\PowerShell\SPBM\SolidFire-SPBM.psm1"
+
+
+# Collect datastore object that you wish to remove
+$datastore = Get-Datastore Engineering-02
+
+# Collect volume object for the selected datastore
+$volume = $datastore | Get-SFVolumeFromDatastore
+
+
+$datastore | Get-DatastoreMountInfo
+
+# Unmount the datastore
+$datastore | Unmount-Datastore
+
+# Check mount info for the datastore (optional)
+$datastore | Get-DatastoreMountInfo
+
+
+# Detach the datastore from ESXi host
+$datastore | Detach-Datastore
+
+# Remove the volume from the SolidFire System
+# This places the volume in the DeletedVolumes space
+$volume | Remove-SFVolume -Confirm:$false
+
+# Optional | Permanently remove volume from SolidFire system
+# This is a PERMANENT OPERATION!
+#Get-SFDeletedVolume | Remove-SFDeletedVolume -Confirm:$false
